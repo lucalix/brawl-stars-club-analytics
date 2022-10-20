f2109ee5c9c1a2b8805091ea44068bab878e3aea
Lucas Calixto <lucas.float@gmail.com>
2022-10-20 19:16:53 -0300

    feat: add endpoint to create club

--- api/services/ClubGroupService.ts
+++ api/services/ClubGroupService.ts
@@ -1,17 +1,17 @@
 
-// import ClubGroup from '../entities/ClubGroup';
+import ClubGroup from '../entities/ClubGroup';
 // import clubGroupRepository from '../repositories/ClubGroupRepository'
 
-// class ClubGroupService {
-//   async create(name: string): Promise<ClubGroup> {
-//     try {
-//       const clubGroupId = await clubGroupRepository.create(name);
+class ClubGroupService {
+	//   async create(name: string): Promise<ClubGroup> {
+	//     // try {
+	//     //   const clubGroupId = await clubGroupRepository.create(name);
 
-//       return { id: clubGroupId, name }
-//     } catch (err) {
-//       throw err;
-//     }
-//   }
-// }
+	//     //   return { id: clubGroupId, name }
+	//     // } catch (err) {
+	//     //   throw err;
+	//     // }
+	//   }
+}
 
-// export default new ClubGroupService()
+export default new ClubGroupService()
