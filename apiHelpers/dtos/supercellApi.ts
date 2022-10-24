71ce0cec6a00f8d9b4906b8a8c28a009993c3c85
Lucas Calixto <lucas.float@gmail.com>
2022-10-23 21:46:19 -0300

    feat: add supercell battle interface dto types

--- apiHelpers/dtos/supercellApi.ts
+++ apiHelpers/dtos/supercellApi.ts
@@ -19,3 +19,36 @@ export interface IClubMemberFromSupercellApi {
     id: number
   }
 }
+
+export interface IMatchesFromSupercellApi {
+  items: IMatchItemFromSupercellApi[]
+}
+
+export interface IMatchItemFromSupercellApi {
+  battleTime: string
+  event: {
+    id: number
+    mode: string
+    map: string
+  }
+  battle: {
+    mode: string
+    type: string
+    result: "victory" | "defeat" | "draw"
+    duration: number
+    trophyChange: number
+    starPlayer: IMatchPlayerFromSupercellApi,
+    teams: IMatchPlayerFromSupercellApi[][]
+  }
+}
+
+export interface IMatchPlayerFromSupercellApi {
+  tag: string
+  name: string
+  brawler: {
+    id: number
+    name: string
+    power: number
+    trophies: number
+  }
+}
